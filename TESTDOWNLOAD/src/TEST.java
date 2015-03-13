679f663acfbbe9f779012d694b36d8d2d9631e39
SAPEvent <SAPEvent@MOWC60219857A.emea.global.corp.sap>
2015-03-13 12:41:15 +0400

    Изменения с другой учетки

--- TESTDOWNLOAD/src/TEST.java
+++ TESTDOWNLOAD/src/TEST.java
@@ -3,5 +3,6 @@ public class TEST {
 	public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Hello world!");
+        System.out.println("Hello GitHub!");
     }
 }
